e956c9f21cc74eb0192dcd838a8ececff86147f0
Tu Nombre <you@example.com>
2026-01-05 07:12:50 -0300

    second commit

---
+++ core-service-java/bank-ia/src/main/java/com/bank/bank_ia/BankIaApplication.java
@@ -0,0 +1,13 @@
+package com.bank.bank_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankIaApplication.class, args);
+	}
+
+}
